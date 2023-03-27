8896cab1f3b8cc2793e43bce16ee7c31cdd9cdc7
shreyvishal <shreya.vishal@eminds.ai>
2023-03-27 16:56:40 +0530

    Fixed:Some Bug

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import "./App.css";
 
 function App() {
   return (
-
     <div className="App">
       <h1>HEllO</h1>
-      <h2>Welcome</h2>
+      <h2>Welcome2</h2>
     </div>
-  )
+  );
 }
 
 export default App;
